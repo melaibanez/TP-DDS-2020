c46b058ec4c3bb26d0e27e398a8500b5f7ac3bf4
ezecoen <ezecoen@gmail.com>
2020-04-09 21:09:43 -0300

    arrancamos aca el tp

---
+++ TP DDS (consola)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_DDS__consola_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Madafakas!");
+        }
+    }
+}
