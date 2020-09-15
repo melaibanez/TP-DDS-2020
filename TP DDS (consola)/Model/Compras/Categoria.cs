458b8562953eb8c72c35e7f736231fac407a491f
ezecoen <ezecoen@gmail.com>
2020-09-15 18:34:16 -0300

    modificacion modelo de objetos y der

--- TP DDS (consola)/Model/Compras/Categoria.cs
+++ TP DDS (consola)/Model/Compras/Categoria.cs
@@ -7,10 +7,9 @@ namespace TP_DDS__consola_.Model.Compras
     public class Categoria
     {
         public int idCategoria { get; set; }
-
         public Criterio criterio { get; set; }
-
         public string nombre { get; set; }
+        public Categoria subCategoria { get; set; }
 
         public Criterio getCriterio() { return criterio; }
     }
