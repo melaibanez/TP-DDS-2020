aeff32ddd51e7ea689c4ef649b4a617f562a1081
Aeiroa <aeiroa@est.frba.utn.edu.ar>
2020-09-21 13:04:14 -0300

    Agrego las validaciones de la dirreccion postal

--- TP_DDS/TP_DDS/Validadores/ValidadorDireccionPostal.cs
+++ TP_DDS/TP_DDS/Validadores/ValidadorDireccionPostal.cs
@@ -15,18 +15,47 @@ namespace TP_DDS.Validadores
 
         public static bool validarDireccionPostal(DireccionPostal dirPos)
         {
-            validarPais(dirPos);
+            string id_P = validarPais(dirPos);
+            if (!id_P.Equals("Pais No Encontrado")){ 
+                validarProvincia(dirPos, id_P);
+            }
             return true;
         }
 
-        public static bool validarPais(DireccionPostal dirPos)
+        public static string validarPais(DireccionPostal dirPos)
         {
             var client = new RestClient("https://api.mercadolibre.com/");
+ 
             var request = new RestRequest("classified_locations/countries/");
             request.RequestFormat = DataFormat.Json;
             var response = client.Get(request).Content;
-            dynamic stuff = JObject.Parse(response);
-            return true;
+            dynamic countries = JArray.Parse(response);
+            string paisComparado = "Uruguay";//dirPos.pais;
+            int i;
+            int cantidadPaises = countries.Count;
+            for (i = 0; i < cantidadPaises; i++)
+            {
+                //Console.WriteLine(countries[i].name);
+                if (countries[i].name == paisComparado)
+                {
+                    //Console.WriteLine(countries[i].id);
+                    return countries[i].id;
+                }
+            }
+            return "Pais No Encontrado";
+
+        }
+
+        public static string validarProvincia(DireccionPostal dirPos, string id_P)
+        {
+            var client = new RestClient("https://api.mercadolibre.com/");
+
+            var request = new RestRequest("classified_locations/countries/" + id_P);
+            request.RequestFormat = DataFormat.Json;
+            var response = client.Get(request).Content;
+            dynamic states = JArray.Parse(response);
+
+
 
         }
     }
