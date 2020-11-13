5becf5b9c951c1b0ff5ead37a08811167ea2931b
Melanie Ibañez <melu_74_12@hotmail.com>
2020-11-13 12:49:10 -0300

    Agrego clase OVPE

--- TP_DDS_MVC/Helpers/VinculadorEgresoIngreso/OVPE.cs
+++ TP_DDS_MVC/Helpers/VinculadorEgresoIngreso/OVPE.cs
@@ -12,16 +12,15 @@ namespace TP_DDS_MVC.Helpers.VinculadorEgresoIngreso
     {
         public override void vincularCompras(List<Egreso> egresos, List<Ingreso> ingresos)
         {
-            List<Egreso> egresosSinVincular = entidad.GetComprasSinIngresoAsignado().Select(compra => compra.egreso).OrderBy(egreso => egreso.montoTotal).ToList();
-            List<Ingreso> ingresosDisponibles = entidad.GetIngresosDisponibles().OrderBy(ingreso => ingreso.monto).ToList();
-            int cantidadEgresos = egresosSinVincular.Count();
-            int cantidadIngresosDisponibles = ingresosDisponibles.Count();
+         
+            int cantidadEgresos = egresos.Count();
+            int cantidadIngresosDisponibles = ingresos.Count();
 
             for (int i = 0; i < cantidadEgresos; i++)
             {
                 for (int j = 0; j < cantidadIngresosDisponibles; j++)
                 {
-                    if (cumpleCondiciones(egresosSinVincular.ElementAt(i), ingresosDisponibles.ElementAt(j)))
+                    if (cumpleCondiciones(egresos.ElementAt(i), ingresos.ElementAt(j)))
                     {
                         asignarEgresoIngreso(egresosSinVincular.ElementAt(i), ingresosDisponibles.ElementAt(j));
                         break; //si ya asigno un egreso a un ingreso pasa al siguiente. Y vuelve a cero J para empezar de nuevo.
