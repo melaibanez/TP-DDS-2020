ad0ccb28329a6b18c99dd571a0ef0429ff41e4e2
Jose Zudaire <zudairejose@yahoo.com.ar>
2020-11-23 17:04:33 -0300

    pusheo los cambios en las vistas

---
+++ TP_DDS_MVC/Controllers/AutenticacionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_DDS_MVC.DAOs;
+
+namespace TP_DDS_MVC.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        // GET: Autenticacion
+        public ActionResult Index()
+        {
+            return View("Autenticacion");
+        }
+
+        public ActionResult Autenticacion()
+        {
+            return View("Autenticacion");
+        }
+        
+    }
+}
---
+++ TP_DDS_MVC/Controllers/EgresosPresupuestosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_DDS_MVC.DAOs;
+
+namespace TP_DDS_MVC.Controllers
+{
+    public class EgresosPresupuestosController : Controller
+    {
+        // GET: AddEgreso
+        public ActionResult Index()
+        {
+            return View("EgresosPresupuestos");
+        }
+
+        public ActionResult EgresosPresupuestos()
+        {
+            return View("EgresosPresupuestos");
+        }
+    }
+}
