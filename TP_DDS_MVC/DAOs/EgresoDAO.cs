e176d2ecaa2e535f44ede3f5f59c4e4667467711
ezecoen <ezecoen@gmail.com>
2020-12-06 20:30:35 -0300

    list presupuesto y detalle presupuesto hechos

--- TP_DDS_MVC/DAOs/EgresoDAO.cs
+++ TP_DDS_MVC/DAOs/EgresoDAO.cs
@@ -47,6 +47,7 @@ namespace TP_DDS_MVC.DAOs
             using (MyDBContext context = new MyDBContext())
             {
                 added = context.Egresos.Add(egreso);
+                context.SaveChanges();
             }
 
             return added;
